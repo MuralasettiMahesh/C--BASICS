e1ed9236cb0585a5e8ba6fd21ec33ea3707b72eb
MuralasettiMahesh <93814010+MuralasettiMahesh@users.noreply.github.com>
2021-11-06 12:14:11 +0530

    Add files via upload

---
+++ maheshmuralasetti.c
@@ -0,0 +1,27 @@
+//printing all arithmatic operations:
+#include<stdio.h>
+void main()
+{
+	int a,b,s,d,p,m;
+	a=10;
+	b=2;
+	
+	s=a+b;
+	printf("sum of %d and %d is %d\n",a,b,s);
+	
+	d=a-b;
+	printf("differance of %d and %d is %d\n",a,b,d);
+	
+	p=a*b;
+	printf("product of %d and %d is %d\n",a,b,p);
+	
+	m=a%b;
+	printf("modular divison of %d and %d is %d\n",a,b,m);
+	
+	
+	
+	
+		
+	
+	
+}
